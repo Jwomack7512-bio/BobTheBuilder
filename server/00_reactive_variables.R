834ba95ef78e14d8babe383cc0a15e83c39272a2
Jwomack7512-bio <78865343+Jwomack7512-bio@users.noreply.github.com>
2022-04-21 01:15:19 -0500

    Added RV for custom differential equations

--- server/00_reactive_variables.R
+++ server/00_reactive_variables.R
@@ -163,6 +163,8 @@ params <- reactiveValues(
 DE <- reactiveValues(
   eqns = vector() #store differential equations
   ,eqns.in.latex  = vector() #store differential equations as latex eqns to print
+  ,custom.diffeq.idx = vector() #keeps track of indices of custom differential eqns
+  ,custom.diffeq = vector() #keeps track of custom entered diffeq
 )
 
 options <- reactiveValues(time.start = 0 
