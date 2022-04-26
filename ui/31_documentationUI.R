782ff591f9480eefe675f46e546bad653a1c9b77
Womack <ju61191@mcw.edu>
2022-04-26 16:17:20 -0500

    changed iframe size to expand with page

--- ui/31_documentationUI.R
+++ ui/31_documentationUI.R
@@ -1,5 +1,6 @@
 TAB_DOCUMENTATION <- tabItem(tabName = "TAB_DOCUMENTATION", 
-                             tags$iframe(style="height:700px; width:100%; scrolling=yes", 
+                             
+                             tags$iframe(style="height:90vh; width:100%; scrolling=yes", 
                                          src="./documentation/Modeler_Documentation.pdf")
                              
 )#end Tabitem
