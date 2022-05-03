093bca9cb3b607a7c485be52356e51099382e117
Jwomack7512-bio <78865343+Jwomack7512-bio@users.noreply.github.com>
2022-05-03 14:50:43 -0500

    cleaned up rhandsometable for IC

--- server/05_modelBuild_ICs.R
+++ server/05_modelBuild_ICs.R
@@ -5,39 +5,6 @@
 # needs to create table that is editable and changes the respectable RVs.
 # should control the parameters: 
 
-output$ICs_DT <- renderDT({
-  DT::datatable(ICs$ICs.table
-                ,editable = list(target = "column", disable = list(columns = c(0,1)))
-                ,class = "cell-border stripe"
-                ,options = list(autoWidth = TRUE
-                                ,pageLength = -1
-                                ,ordering = FALSE
-                                ,columnDefs = list(list(width = "60%", targets = 3),
-                                                   list(width = "20%", targets = 1),
-                                                   list(className = 'dt-center', targets = c(1,2)),
-                                                   list(className = 'dt-left', targets = 3)
-                                )
-                                ,dom = 'ft'
-                )
-  )
-
-})
-
-proxy_ICs_table = dataTableProxy("ICs_DT")
-
-observeEvent(input$ICs_DT_cell_edit, {
-  info = input$ICs_DT_cell_edit
-  #str(info)
-  ICs$ICs.table <- editData(ICs$ICs.table, info)
-  replaceData(proxy_ICs_table, ICs$ICs.table, resetPaging = FALSE)
-
-  ICs$vals <- ICs$ICs.table[, 2]
-  ICs$comments <- ICs$ICs.table[, 3]
-  
-  loop$ICs <- ICs$ICs.table
-  
-})
-
 output$ICs_RHT <- renderRHandsontable({
   rhandsontable(ICs$ICs.table,
                 rowHeaders = NULL,
@@ -57,9 +24,6 @@ output$ICs_RHT <- renderRHandsontable({
              } else {
               td.style.background = 'white';
              };
-             if (col == 2 || col == 1) {
-             td.style.background = '#f5f5dd';
-             }
            }") %>%
     hot_col("Variable", readOnly = TRUE) %>%
     #hot_col("Description", halign = "htLeft", valign = "htMiddle") %>%
@@ -70,37 +34,6 @@ output$ICs_RHT <- renderRHandsontable({
     hot_validate_numeric(col = 2, min = 0)
 })
 
-output$ICs_RHT_2 <- renderRHandsontable({
-  rhandsontable(ICs$ICs.table,
-                rowHeaders = NULL,
-                colHeaderWidth = 100,
-                stretchH = "all"
-  ) %>%
-    hot_cols(#colWidth = c(30, 30, 90),
-      manualColumnMove = FALSE,
-      manualColumnResize = TRUE,
-      halign = "htCenter",
-      valign = "htMiddle",
-      renderer = "
-           function (instance, td, row, col, prop, value, cellProperties) {
-             Handsontable.renderers.NumericRenderer.apply(this, arguments);
-             if (row % 2 == 0) {
-              td.style.background = '#f9f9f9';
-             } else {
-              td.style.background = 'white';
-             };
-             //if (col == 2 || col == 1) {
-             //td.style.background = '#f5f5dd';
-             //}
-           }") %>%
-    hot_col("Variable", readOnly = TRUE) %>%
-    #hot_col("Description", halign = "htLeft", valign = "htMiddle") %>%
-    hot_rows(rowHeights = 40) %>%
-    hot_context_menu(allowRowEdit = FALSE,
-                     allowColEdit = FALSE
-    ) %>%
-    hot_validate_numeric(col = 2, min = 0)
-})
 
 observeEvent(input$ICs_RHT$changes$changes, {
   xi = input$ICs_RHT$changes$changes[[1]][[1]]
@@ -116,59 +49,4 @@ observeEvent(input$ICs_RHT$changes$changes, {
   jPrint(ICs$comments)
   jPrint(ICs$ICs.table)
   loop$ICs <- ICs$ICs.table
-})
-
-
-################################################################################
-
-
-
-
-output$ICs_UI <- renderUI({
-  req(input$createVar_addVarToList)
-  number_var = length(vars$species)
-  
-
-  fluidRow(column(width = 4
-                  ,lapply(seq(number_var), function(i){
-                    textInput(inputId = paste0("IC_", as.character(i))
-                              ,label = paste(vars$species[i], "initial value:")
-                              ,value = ifelse(ICs$first.IC.stored, ICs$vals[i], "0"))
-                  }))
-           # ,column(width=1
-           #         ,lapply(seq(number_parameters), function(i){
-           #           checkboxInput(inputId=paste0("parameter_check_unknown_", as.character(i))
-           #                     ,label="Value Unknown"
-           #                     ,value = FALSE)
-           #         }))
-           ,column(width = 6
-                   ,lapply(seq(number_var), function(i){
-                     textInput(inputId = paste0("ICs_description_", as.character(i))
-                               ,label = "Comment"
-                               ,value = ifelse(ICs$first.IC.stored, ICs$comments[i], ""))
-                   })
-           )
-  ) #end fluidRow
-})
-
-observeEvent(input$ICs_store_ICs, {
-  ICs$first.IC.stored = TRUE #set boolean so program knows button to store was pressed so render ui will load data instead of blank descriptions
-  num_ICs <- length(vars$species)
-  
-  IC_values <- vector()
-  IC_comments <- vector()
-  for (i in seq(num_ICs)) {
-    single_value <- eval(parse(text = paste0("input$IC_", as.character(i))))
-    IC_values <- append(IC_values, single_value)
-    
-    single_comment <- eval(parse(text = paste0("input$ICs_description_", as.character(i)))) #evaluate value in textinput
-    IC_comments <- append(IC_comments, single_comment) #append comments to vector
-  }
-  IC_values <- paste(IC_values, sep = " ")
-  
-  ICs$vals <- as.numeric(IC_values)
-  ICs$comments <- IC_comments
-  
-  session$sendCustomMessage(type = 'testmessage',
-                            message = 'ICs Stored')
 })
--- ui/05_model_ICs_ui.R
+++ ui/05_model_ICs_ui.R
@@ -6,17 +6,6 @@
 #  MCW, Milwaukee, WI, USA
 #-------------------------------------------------------------------------
 
-TAB_ICs <- tabItem(tabName = "TAB_ICs"
-                   ,DTOutput("ICs_DT"),
-                   #hr(),
-                   br(),
-                   br(),
-                   rHandsontableOutput("ICs_RHT"),
-                   br(),
-                   br(),
-                   rHandsontableOutput("ICs_RHT_2")
-                   # tags$style(type="text/css", "#ICs_RHT th {font-weight:bold;
-                   #                                           background-color:#3c8dbc;
-                   #                                           color: white;
-                   #                                           height: 35px;}")
+TAB_ICs <- tabItem(tabName = "TAB_ICs",
+                   rHandsontableOutput("ICs_RHT")
                    )
