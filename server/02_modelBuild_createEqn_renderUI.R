85d63e75ab24e63521ba625a40753cf4ffbc0ca9
Womack <ju61191@mcw.edu>
2022-05-18 16:01:44 -0500

    part of waiter in last commit

--- server/02_modelBuild_createEqn_renderUI.R
+++ server/02_modelBuild_createEqn_renderUI.R
@@ -1,11 +1,14 @@
 # This script holds the renderUIs for the equation building suite
+# w.load.MA.vars <- Waiter$new(id = "eqnCreate_equationBuilder_chem")
+# w.load.MA.vars$show()
+# w.load.MA.vars$hide()
 
 output$eqnCreate_equationBuilder_chem <- renderUI({
   number_RHS_equations = as.numeric(input$eqnCreate_num_of_eqn_RHS)
   number_LHS_equations = as.numeric(input$eqnCreate_num_of_eqn_LHS)
   number_forward_regulators = as.numeric(input$eqn_options_chem_num_forward_regulators)
   number_reverse_regulators = as.numeric(input$eqn_options_chem_num_reverse_regulators)
-  
+  Sys.sleep(0.5)
   div(
     fluidRow(
       column(
@@ -147,6 +150,7 @@ output$eqnCreate_equationBuilder_chem <- renderUI({
     ),
 
   )#end div
+ 
 })
 
 output$eqnCreate_equationBuilder_enzyme <- renderUI({
