fe4c13fbe39347b6dfa75b7dbadb1301daf5928e
Jwomack7512-bio <78865343+Jwomack7512-bio@users.noreply.github.com>
2022-11-15 17:25:07 -0600

    Updated parameter estimation UI to look much better

--- ui/51_parameter_estimination_UI.R
+++ ui/51_parameter_estimination_UI.R
@@ -1,23 +1,40 @@
 Tab_Parameter_Estimation <-
-  tabItem(tabName = "Tab_Parameter_Estimation", 
-          "Parameter Estimation",
+  tabItem(tabName = "Tab_Parameter_Estimation",
+  # First Box - Import Data
+          box(
+            title = "Import Observational Data",
+            width = 12,
+            fluidRow(
+              column(
+                width = 4,
+                fileInput("pe_obs_data",
+                          "Import Observed Data")
+              ),
+              column(
+                width = 4,
+                offset = 1,
+                div(style = "display:inline-block; padding-top:35px;",
+                    checkboxInput(
+                      inputId = "pe_show_imported_datatable",
+                      label = "View Table",
+                      value = FALSE
+                    )
+                  )
+              )
+            ),
+            conditionalPanel(
+              condition = "input.pe_show_imported_datatable",
+              rHandsontableOutput(
+                outputId = "pe_import_data_table"
+              )
+            )
+          ),
+  # Box 2: Select Parameters to Estimate
           fluidRow(
             column(
-              width = 3,
+              width = 12,
               box(
-                width = 12,
-                fileInput("pe_obs_data",
-                          "Import Observed Data"),
-                radioButtons(
-                  inputId = "pe_view_import",
-                  label = "View:",
-                  choices = c("Plot", "Table")
-                  )
-                )
-              ),
-            column(
-              width = 9,
-              box(
+                title = "Select Parameters to Estimate",
                 width = 12,
                 fluidRow(
                   column(
@@ -28,45 +45,70 @@ Tab_Parameter_Estimation <-
                       choices = c(),
                       multiple = TRUE
                     )
+                  ),
+                  column(
+                    width = 8,
+                    rHandsontableOutput(
+                      outputId = "pe_parameter_value_table"
+                    )
+                  )
+                 ),
+                fluidRow(
+                  column(
+                    width = 1,
+                    offset = 11,
+                    actionBttn(
+                      inputId = "pe_run_parameter_estimation",
+                      label = "Run"
+                    )
                   )
                 )
               )
             )
-            ),
-          fluidRow(
-            rHandsontableOutput(
-              outputId = "pe_parameter_value_table"
-            ),
-            actionBttn(
-              inputId = "pe_run_parameter_estimation",
-              label = "Run"
-              )
           ),
-          fluidRow( 
+  # Box 3: View Results of Parameter Estimation
+          fluidRow(
             column(
               width = 12,
-              conditionalPanel(
-                condition = "input.pe_view_import == 'Table'",
-                rHandsontableOutput(
-                  outputId = "pe_import_data_table"
-                )
-              ),
-              conditionalPanel(
-                condition = "input.pe_view_import == 'Plot'", 
-                plotOutput(
-                  outputId = "pe_parameter_estimation_plot"
+              box(
+                title = "Estimation Results",
+                width = 12,
+                fluidRow(
+                  column(
+                    width = 4,
+                    verbatimTextOutput(
+                      outputId = "pe_logs",
+                      placeholder = TRUE
+                    )
+                  ),
+                  column(
+                    width = 8,
+                    plotOutput(
+                      outputId = "pe_parameter_estimation_plot"
+                    )
+                  )
+                ),
+                fluidRow(
+                  column(
+                    width = 1,
+                    offset = 11,
+                    actionBttn(
+                      inputId = "pe_store_estimated_parameters",
+                      label = "Store"
+                    )
+                  )
                 )
               )
             )
-          ),
-          actionBttn(
-            inputId = "pe_store_estimated_parameters",
-            label = "Store"
-          ),
-          verbatimTextOutput(
-            outputId = "pe_logs"
           )
-          )
+            # tags$head(
+            #   tags$style(HTML("
+            #                   #pe_logs {
+            #                   background-color: white;
+            #                   }"
+            #                   ))
+            # )
+        )
   
 
 
