6081bc31cdd503110093facdb6beb9318b7dd2ff
Jwomack7512-bio <78865343+Jwomack7512-bio@users.noreply.github.com>
2022-12-09 12:22:38 -0600

    cleaned server script

--- server.R
+++ server.R
@@ -5,22 +5,18 @@
 #  Last Update: January 5, 2021
 #  MCW, Milwaukee, WI, USA
 #-------------------------------------------------------------------------
-waiting_screen <- tagList(
-  spin_pong(),
-  br(),
-  h4("Loading Model..."),
-)
+waiting_screen <- tagList(spin_pong(),
+                          br(),
+                          h4("Loading Model..."),)
 
-button_store <- tagList(
-  spin_loaders(32),
-  h4("Overwriting Parameters")
-)
+button_store <- tagList(spin_loaders(32),
+                        h4("Overwriting Parameters"))
 
 server <- shinyServer(function(input, output, session) {
   #allows for the import of bigger data frames
   options(shiny.maxRequestSize = 30000 * 1024 ^ 2)
   #options(shiny.sanitize.errors = TRUE)
- 
+  
   #helpers here (generating_diffEqs_functions)
   source("./server/helpers.R")
   source("./server/helper_createEqns.R")
@@ -32,21 +28,25 @@ server <- shinyServer(function(input, output, session) {
   source("./server/helper_id_generator.R")
   source("./server/helper_recalc_text_equations.R")
   
-  table.header<- reactiveValues(bg = "#3c8dbc",
-                                color = 'white')
+  table.header <- reactiveValues(bg = "#3c8dbc",
+                                 color = 'white')
   
   source(file.path("server", "00_reactive_variables.R"), local = TRUE)$value
   source(file.path("server", "01_modelBuild_createVar.R"), local = TRUE)$value
   source(file.path("server", "02_modelBuild_createEqn.R"), local = TRUE)$value
-  source(file.path("server", "02_modelBuild_createEqn_renderUI.R"), local = TRUE)$value
-  source(file.path("server", "02_modelBuild_createEqn_textEqn_mathjax.R"), local=TRUE)$value
-  source(file.path("server", "02_modelBuild_createEqn_mathjax.R"), local = TRUE)$value
-  source(file.path("server", "02_modelBuild_createEqn_edit.R"), local = TRUE)$value
+  source(file.path("server", "02_modelBuild_createEqn_renderUI.R"),
+         local = TRUE)$value
+  source(file.path("server", "02_modelBuild_createEqn_textEqn_mathjax.R"),
+         local = TRUE)$value
+  source(file.path("server", "02_modelBuild_createEqn_mathjax.R"),
+         local = TRUE)$value
+  source(file.path("server", "02_modelBuild_createEqn_edit.R"),
+         local = TRUE)$value
   source(file.path("server", "03_modelBuild_IO.R"), local = TRUE)$value
   source(file.path("server", "04_modelBuild_params.R"), local = TRUE)$value
   source(file.path("server", "05_modelBuild_ICs.R"), local = TRUE)$value
   source(file.path("server", "06_modelBuild_diffeqn.R"), local = TRUE)$value
-
+  
   source(file.path("server", "11_run_execute.R"), local = TRUE)$value
   source(file.path("server", "12_run_post_processing.R"), local = TRUE)$value
   source(file.path("server", "13_0_run_lineplot.R"), local = TRUE)$value
@@ -58,18 +58,23 @@ server <- shinyServer(function(input, output, session) {
   
   source(file.path("server", "21_export.R"), local = TRUE)$value
   # #additional source pages
-  source(file.path("server", "server_load_model.R"), local = TRUE)$value #controls the load function server in the sidebar
+  # source(file.path("server", "server_load_model.R"), local = TRUE)$value #controls the load function server in the sidebar
+  source(file.path("server", "server_load_script.R"), local = TRUE)$value
   
   output$css_themes <- renderUI({
-    tags$head(
-      if (input$css_selector == "Default") {
-        tags$link(rel = 'stylesheet', type = 'text/css', href = 'css/default.css')
-      }  else if (input$css_selector == "RoyalBlue") {
-        tags$link(rel = "stylesheet", type = "text/css", href = "css/royalBlue.css")
-      } else if (input$css_selector == "Night") {
-        tags$link(rel = "stylesheet", type = "text/css", href = "css/night.css")
-      }
-    )
+    tags$head(if (input$css_selector == "Default") {
+      tags$link(rel = 'stylesheet',
+                type = 'text/css',
+                href = 'css/default.css')
+    }  else if (input$css_selector == "RoyalBlue") {
+      tags$link(rel = "stylesheet",
+                type = "text/css",
+                href = "css/royalBlue.css")
+    } else if (input$css_selector == "Night") {
+      tags$link(rel = "stylesheet",
+                type = "text/css",
+                href = "css/night.css")
+    })
   })
   
   # This changes the colors of the generated DT table UI
