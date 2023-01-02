c3dfbe41123368f373764645ae47fe34eb80badc
Jwomack7512-bio <78865343+Jwomack7512-bio@users.noreply.github.com>
2023-01-02 12:12:27 -0600

    added debug tab

---
+++ server/debug.R
@@ -0,0 +1,16 @@
+
+
+# View IDs ---------------------------------------------------------------------
+observeEvent(input$view_ids, {
+  print(id$id.variables)
+  print(id$id.parameters)
+  print(id$id.equations)
+  print(id$id.diffeq)
+  print(id$id.compartments)
+})
+
+# View Variables ---------------------------------------------------------------
+observeEvent(input$view_variables, {
+  print(vars$var.info)
+  print(vars$compartments.info)
+})
