d94c27e73e930ed5e82bb4687df00653bd25480a
Jwomack7512-bio <jwomack@mcw.edu>
2023-01-13 10:27:08 -0600

    moved strsplits to help script

--- server/helpers.R
+++ server/helpers.R
@@ -2,6 +2,20 @@
 # jPrint
 # RenameParameterVector
 # VarToLatexForm
+
+strsplits <- function(x, splits, ...)
+  #splits string on multiple inputs
+  #used strsplits(a, c(",", " ")) for space and comma splits of c
+  #returns vector of split variables
+  #https://stackoverflow.com/questions/10738729/r-strsplit-with-multiple-unordered-split-arguments
+{
+  for (split in splits)
+  {
+    x <- unlist(strsplit(x, split, ...))
+  }
+  return(x[!x == ""]) # Remove empty values
+}
+
 PrintVar <- function(variable,
                      sameLine = TRUE) {
   # Prints a variable and tells what that variable is
