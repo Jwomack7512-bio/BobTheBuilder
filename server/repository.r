cf1827d9a3c45a7c0d2240d612ed13dcbafe7564
Womack <ju61191@mcw.edu>
2023-05-19 13:22:11 -0500

    feature: model name/description pulled from repo

--- server/repository.r
+++ server/repository.r
@@ -16,14 +16,30 @@ selected_base_model <- reactive({
 
 
 # TextOuput to display title of model 
-output$TO_repos_model_name <- renderPrint({
+output$TO_repos_model_name <- renderText({
+  mod.name <- selected_base_model()$model.name
+  header <- "Model Name:\n"
+  print(paste0(header, mod.name))
   
-  selected_base_model()
 })
 
+
+output$TO_repos_model_description <- renderText({
+  descript <- selected_base_model()$model.description
+  header <- "Description:\n"
+  print(paste0(header, descript))
+})
 
 # On Load Model Button Press, Load Model to Main Application
 observeEvent(input$bttn_load_model_from_base_repo, {
   rv.LOADBUTTONS$LB.button.name <- "Load_base_model"
   rv.LOADBUTTONS$LB.count <- rv.LOADBUTTONS$LB.count + 1
+  
+  # Change NavBar to Original Panel
+  # updateSidebar(session, "TAB_VAR_CREATE", selected = 1)
+  updateTabItems(
+    session,
+    inputId = "sidebar_tabs",
+    selected = "TAB_VAR_CREATE"
+  )
 })
