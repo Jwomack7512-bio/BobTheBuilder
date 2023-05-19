340fa058655ae3e6ce2c50451a84fd49d4fd4711
Womack <ju61191@mcw.edu>
2023-05-19 13:24:53 -0500

    added ui and styling for model repository page

--- ui/repository_ui.R
+++ ui/repository_ui.R
@@ -3,30 +3,32 @@ TAB_MODEL_REPOSITORY <-
     tabName = "TAB_MODEL_REPOSITORY",
     fluidRow(
       column(
-        width = 4,
+      width = 4,
+      div(
+        style = "float: left",
         selectInput(
           inputId = "SI_repos_base_choices",
           label = "Select Model To Load:",
           choices <- c(),
           selectize = FALSE,
           size = 12
+        ),
+        actionButton(
+          inputId = "bttn_load_model_from_base_repo",
+          label = "Load Model",
+          style = "float: right;"
         )
+      )
       ), 
       column(
         width = 8,
         verticalLayout(
-          fluidRow(
-            column(
-              width = 12,
-              align = "right",
-              actionButton(
-                inputId = "bttn_load_model_from_base_repo",
-                label = "Load Model"
-              )
-            )
-          ),
-          textOutput(outputId = "TO_repos_model_name"),
-          textOutput(outputId = "TO_repos_model_description")
+          verbatimTextOutput(outputId = "TO_repos_model_name"),
+          verbatimTextOutput(outputId = "TO_repos_model_description"),
+          tags$style(
+            type='text/css', 
+            '#TO_repos_model_description {white-space: pre-wrap;}'
+          )
         )
       )
     )
